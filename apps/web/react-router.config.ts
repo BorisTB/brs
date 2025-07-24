58fca0b759edab60650f696447624b2eff1daab6
Boris Polak <18208654+BorisTB@users.noreply.github.com>
2025-07-24 23:18:21 +0200

    build: do pice s celym cloudflarom

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -2,8 +2,5 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
   ssr: true,
-  buildDirectory: '../../dist/apps/web',
-  future: {
-    unstable_viteEnvironmentApi: true
-  }
+  buildDirectory: '../../dist/apps/web'
 } satisfies Config;
